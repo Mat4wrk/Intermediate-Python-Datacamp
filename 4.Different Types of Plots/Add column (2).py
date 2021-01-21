41ec2bb34b3c3efd3472a54263e20d2073bed70a
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-21 14:29:16 +0700

    Delete 4.Different Types of Plots directory

--- 4.Different Types of Plots/Add column (2).py
+++
@@ -1,8 +0,0 @@
-# Import cars data
-import pandas as pd
-cars = pd.read_csv('cars.csv', index_col = 0)
-
-# Use .apply(str.upper)
-for lab, row in cars.iterrows() :
-    cars.loc[lab, "COUNTRY"] = row["country"].upper()
-    cars["COUNTRY"] = cars["country"].apply(str.upper)
--- 4.Different Types of Plots/Loop over DataFrame (2).py
+++
@@ -1,7 +0,0 @@
-# Import cars data
-import pandas as pd
-cars = pd.read_csv('cars.csv', index_col = 0)
-
-# Adapt for loop
-for lab, row in cars.iterrows() :
-        print(str(lab) + ":" + " " + str(row["cars_per_cap"]))
