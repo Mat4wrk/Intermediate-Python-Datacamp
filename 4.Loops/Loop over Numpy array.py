5f5461e66e9fda6fc4f9f00cc22d6f2e144a25e4
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-21 14:29:57 +0700

    Add files via upload

---
+++ 4.Loops/Loop over Numpy array.py
@@ -0,0 +1,10 @@
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for x in np_height:
+  print(str(x) + " inches")
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball):
+    print(x)
